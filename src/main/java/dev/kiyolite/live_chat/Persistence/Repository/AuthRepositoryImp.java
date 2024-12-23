670f6a2733f00190670862927b2f92deab658e23
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-12-23 13:14:48 -0500

    [add] authRepository to manage user authentication

---
+++ src/main/java/dev/kiyolite/live_chat/Persistence/Repository/AuthRepositoryImp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.kiyolite.live_chat.Persistence.Repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author soyky
+ */
+@Repository
+public class AuthRepositoryImp implements AuthRepository{
+    
+}
