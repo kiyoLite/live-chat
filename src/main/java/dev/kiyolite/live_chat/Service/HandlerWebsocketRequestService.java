0d00e7e1d7d5ff016e4a4155fb90587557a748d3
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-12-23 13:20:21 -0500

    [feat] add method to HandlerWebsocketRequestService to save a user in websocket session storage

--- src/main/java/dev/kiyolite/live_chat/Service/HandlerWebsocketRequestService.java
+++ src/main/java/dev/kiyolite/live_chat/Service/HandlerWebsocketRequestService.java
@@ -4,6 +4,10 @@
  */
 package dev.kiyolite.live_chat.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.kiyolite.live_chat.Entities.WebsocketRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,6 +16,21 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class HandlerWebsocketRequestService {
+    
+    private ObjectMapper objMapper;
+    
+    static long connectUser(WebsocketRequest request) {
+        String payload = request.payload();
+        return 0;
+    }
+
+    @Autowired
+    public void setObjMapper(ObjectMapper objMapper) {
+        this.objMapper = objMapper;
+    }
+    
+    
+    
     
     
 }
--- src/main/java/dev/kiyolite/live_chat/Service/WebsocketService.java
+++ src/main/java/dev/kiyolite/live_chat/Service/WebsocketService.java
@@ -4,8 +4,17 @@
  */
 package dev.kiyolite.live_chat.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.kiyolite.live_chat.Entities.DB.User;
+import dev.kiyolite.live_chat.Entities.WebsocketRequest;
+import dev.kiyolite.live_chat.Enums.WebsocketRequestType;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 /**
@@ -14,5 +23,46 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
  */
 @Service
 public class WebsocketService extends TextWebSocketHandler{
+    private ConcurrentHashMap<WebSocketSession,Long> connectUsers = new ConcurrentHashMap<>();
+    private HandlerWebsocketRequestService requestHandler; 
+    private ObjectMapper ObjectMapper;
+    
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        connectUsers.put(session, null);
+    }
+    
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        connectUsers.remove(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        WebsocketRequest request = ObjectMapper.convertValue(message.getPayload(), WebsocketRequest.class);
+        WebsocketRequestType requestType = WebsocketRequestType.valueOf(request.websocketRequestType());
+        switch(requestType){
+            case CONNECT:
+                long userId = HandlerWebsocketRequestService.connectUser(request);
+                connectUsers.put(session,userId);
+                break;
+        }
+    }
+
+    
+    
+    public void setObjectMapper(ObjectMapper ObjectMapper) {
+        this.ObjectMapper = ObjectMapper;
+    }
+
+    
+    
+    @Autowired
+    public void setRequestHandler(HandlerWebsocketRequestService requestHandler) {
+        this.requestHandler = requestHandler;
+    }
+    
+    
+
     
 }
