f9e94f0a392c8908e8967ef547fc1aedeca49a35
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-12-22 22:39:03 -0500

    [feat] create websocketService to manage request socket request

---
+++ src/main/java/dev/kiyolite/live_chat/Service/WebsocketService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.kiyolite.live_chat.Service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ *
+ * @author soyky
+ */
+@Service
+public class WebsocketService extends TextWebSocketHandler{
+    
+}
