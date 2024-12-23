9dd901c8b8c1be5bec33b4e05418268ae7a9e1fe
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-12-22 22:38:28 -0500

    [feat] create a websocketConfig and define endpoint connection

---
+++ src/main/java/dev/kiyolite/live_chat/Config/WebsocketConfig.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.kiyolite.live_chat.Config;
+
+import dev.kiyolite.live_chat.Service.WebsocketService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ *
+ * @author soyky
+ */
+@EnableWebSocket
+@Configuration
+public class WebsocketConfig implements WebSocketConfigurer{
+    
+    private WebsocketService chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "/chat");
+    }
+    
+    
+    
+}
