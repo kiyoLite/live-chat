79da71ec4756634488b7e8ef5388f8d5db6d3fa4
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-12-25 21:19:16 -0500

    [add] jwtService to manage jwt and application security

---
+++ src/main/java/dev/kiyolite/live_chat/Service/JWTService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.kiyolite.live_chat.Service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ *
+ * @author soyky
+ */
+public class JWTService {
+    
+    public boolean isValidToken(UserDetails userName, String token){
+        return false;
+    }
+
+    String getSubject(String token) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
