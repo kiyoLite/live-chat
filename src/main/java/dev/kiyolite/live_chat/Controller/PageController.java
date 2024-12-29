9ee5734ebdea18f01a73eeb6b6dd6c2434d23e27
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-12-29 17:41:21 -0500

    [add] create controller to loading html page with thymeleaf

---
+++ src/main/java/dev/kiyolite/live_chat/Controller/PageController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.kiyolite.live_chat.Controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author soyky
+ */
+@Controller
+public class PageController {
+    
+}
