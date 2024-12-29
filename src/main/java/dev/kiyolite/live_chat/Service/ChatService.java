0e6d6e9959a77f11735bc1db3ac2648dca1ea1f3
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-12-29 17:45:34 -0500

    [feat] overcharge method to getContacts from user, instead required a user object just need the user name as argument

--- src/main/java/dev/kiyolite/live_chat/Service/ChatService.java
+++ src/main/java/dev/kiyolite/live_chat/Service/ChatService.java
@@ -5,10 +5,19 @@
 package dev.kiyolite.live_chat.Service;
 
 import dev.kiyolite.live_chat.Entities.ChatWrapper;
+import dev.kiyolite.live_chat.Entities.ContactAdditionRequest;
+import dev.kiyolite.live_chat.Entities.DB.Chat;
 import dev.kiyolite.live_chat.Entities.DB.User;
+import dev.kiyolite.live_chat.Persistence.DAO.ChatDAO;
+import dev.kiyolite.live_chat.Persistence.DAO.UserDAO;
 import dev.kiyolite.live_chat.Persistence.Repository.ChatRepository;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  *
@@ -16,14 +25,61 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ChatService {
-    
+
     private ChatRepository chatRepository;
-    
-    public List<ChatWrapper> getContactsFromUser(User user){
+    private UserDAO userDAO;
+    private ChatDAO chatDAO;
+
+    public ResponseEntity<List<ChatWrapper>> getContactsFromUser(User user) {
         long userId = user.getId();
-        List<ChatWrapper> contacts =chatRepository.getContacts(userId);
-        return contacts;
+        List<ChatWrapper> contacts = chatRepository.getContacts(userId);
+        ResponseEntity<List<ChatWrapper>> response = new ResponseEntity<>(contacts, HttpStatus.ACCEPTED);
+        return response;
+    }
+
+    public ResponseEntity<List<ChatWrapper>> getContactsFromUser(String userName) {
+        Optional<User> possibleUser = userDAO.findByUserName(userName);
+        ResponseEntity<List<ChatWrapper>> response;
+        response = possibleUser.isPresent()
+                ? getContactsFromUser(possibleUser.get()) :
+                new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        return response;
+
+    }
+
+    public ResponseEntity<String> addContact(ContactAdditionRequest contactRequest, long userIdCreatorRequest) {
+        String contactName = contactRequest.contactName();
+        User creatorRequest = userDAO.findById(userIdCreatorRequest).get();
+        Optional<User> possibleContact = userDAO.findByUserName(contactName);
+        User contact = possibleContact.orElseGet(() -> null);
+        ResponseEntity<String> response;
+
+        if (contact == null) {
+            response = new ResponseEntity<>("user doesn't found", HttpStatus.NOT_FOUND);
+            return response;
+        }
+
+        Chat chat = new Chat(contact, creatorRequest);
+        chatDAO.save(chat);
+        response = new ResponseEntity<String>("contact was added", HttpStatus.ACCEPTED);
+        return response;
+    }
+
+    @Autowired
+    public void setChatRepository(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
+
+    @Autowired
+    public void setUserDAO(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Autowired
+    public void setChatDAO(ChatDAO chatDAO) {
+        this.chatDAO = chatDAO;
     }
     
     
+
 }
