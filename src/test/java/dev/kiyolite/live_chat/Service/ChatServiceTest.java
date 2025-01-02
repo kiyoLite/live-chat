787340f4ca21a819d8f9e7e12ae106e7e59d27f5
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-01 19:17:23 -0500

    [test] create severals test to checkout get contact methods

--- src/test/java/dev/kiyolite/live_chat/Service/ChatServiceTest.java
+++ src/test/java/dev/kiyolite/live_chat/Service/ChatServiceTest.java
@@ -4,24 +4,32 @@
  */
 package dev.kiyolite.live_chat.Service;
 
+import dev.kiyolite.live_chat.Entities.ChatWrapper;
+import dev.kiyolite.live_chat.Entities.ContactAdditionRequest;
 import dev.kiyolite.live_chat.Entities.DB.Chat;
 import dev.kiyolite.live_chat.Entities.DB.Credential;
 import dev.kiyolite.live_chat.Entities.DB.Message;
 import dev.kiyolite.live_chat.Entities.DB.User;
+import dev.kiyolite.live_chat.Enums.MessageStatus;
 import dev.kiyolite.live_chat.Enums.Rol;
 import dev.kiyolite.live_chat.Persistence.DAO.ChatDAO;
 import dev.kiyolite.live_chat.Persistence.DAO.CredentialDAO;
 import dev.kiyolite.live_chat.Persistence.DAO.MessageDAO;
 import dev.kiyolite.live_chat.Persistence.DAO.UserDAO;
+import jakarta.security.auth.message.callback.PrivateKeyCallback;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 /**
  *
@@ -80,4 +88,64 @@ public class ChatServiceTest {
         messageDAO.deleteAll(messages);
     }
 
+    @Test
+    public void getAllContactsFromUserWioutContacts() {
+        ResponseEntity<List<ChatWrapper>> response = service.getContactsFromUser(userOne);
+        List<ChatWrapper> contacts = response.getBody();
+
+        int expectTotalContacts = 0;
+        int obtainTotalContacts = contacts.size();
+        Assertions.assertEquals(expectTotalContacts, obtainTotalContacts);
+    }
+
+    @Test
+    public void getAllContctsFromUser() {
+        Message messageOne = new Message("hello this is a proof", Calendar.getInstance(), chat, userTwo, MessageStatus.READ);
+        messages = List.of(messageOne);
+        messageDAO.saveAll(messages);
+
+        ResponseEntity<List<ChatWrapper>> response = service.getContactsFromUser(userOne);
+        List<ChatWrapper> contacts = response.getBody();
+
+        int expectTotalContacts = 1;
+        int obtainTotalContacts = contacts.size();
+        Assertions.assertEquals(expectTotalContacts, obtainTotalContacts);
+        String expectContactName = userTwo.getUserName();
+        String obtainContactName = contacts.get(0).contactName();
+        Assertions.assertEquals(expectContactName, obtainContactName);
+    }
+
+    @Test
+    public void getAllContactsByUserName() {
+        Message messageOne = new Message("hello this is a proof", Calendar.getInstance(), chat, userTwo, MessageStatus.READ);
+        messages = List.of(messageOne);
+        messageDAO.saveAll(messages);
+
+        String userNameQuery = userOne.getUserName();
+        ResponseEntity<List<ChatWrapper>> response = service.getContactsFromUser(userNameQuery);
+        List<ChatWrapper> contacts = response.getBody();
+
+        boolean isSuccessResponse = response.getStatusCode().is2xxSuccessful();
+        Assertions.assertTrue(isSuccessResponse);
+        int expectTotalContacts = 1;
+        int obtainTotalContacts = contacts.size();
+        Assertions.assertEquals(expectTotalContacts, obtainTotalContacts);
+        String expectContactName = userTwo.getUserName();
+        String obtainContactName = contacts.get(0).contactName();
+        Assertions.assertEquals(expectContactName, obtainContactName);
+    }
+
+    @Test
+    public void getAllContactsByWrongUserName() {
+        Message messageOne = new Message("hello this is a proof", Calendar.getInstance(), chat, userTwo, MessageStatus.READ);
+        messages = List.of(messageOne);
+        messageDAO.saveAll(messages);
+
+        String userNameQuery = userOne.getUserName() + "Error";
+        ResponseEntity<List<ChatWrapper>> response = service.getContactsFromUser(userNameQuery);
+
+        int expectHtppStatusCode = 404;
+        int obtainHttpStatusCode = response.getStatusCode().value();
+        Assertions.assertEquals(expectHtppStatusCode, obtainHttpStatusCode);
+    }
 }
