fc3b79597c57b4b601cfa69b968a9e707a5ff584
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-06 20:29:11 -0500

    [feat] add test to connection method in case is not possible establish connection

--- src/test/java/dev/kiyolite/live_chat/Service/HandlerWebsocketRequestServiceTest.java
+++ src/test/java/dev/kiyolite/live_chat/Service/HandlerWebsocketRequestServiceTest.java
@@ -4,11 +4,18 @@
  */
 package dev.kiyolite.live_chat.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.kiyolite.live_chat.Entities.AuthLogin;
+import dev.kiyolite.live_chat.Entities.WebsocketRequest;
+import dev.kiyolite.live_chat.Enums.WebsocketRequestType;
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +34,8 @@ public class HandlerWebsocketRequestServiceTest {
 
     @Autowired
     WebsocketService WebsocketService;
+    @Autowired
+    ObjectMapper objMapper;
     private CompletableFuture<TextMessage> futureResponse;
     private WebSocketSession session;
 
@@ -52,6 +61,23 @@ public class HandlerWebsocketRequestServiceTest {
     @AfterEach
     public void tearDownTest() {
         futureResponse = null;
+    }
+    
+    @Test
+    public void NotConnect() throws JsonProcessingException, IOException, InterruptedException, ExecutionException {
+        WebsocketService.getConnectUsers().put(session, 0L);
+        String wrongAuthToken = "this.is.wrongToken"; 
+        AuthLogin authUser = new AuthLogin(wrongAuthToken);
+        WebsocketRequest websocketRequest = new WebsocketRequest(WebsocketRequestType.CONNECT.name(), objMapper.writeValueAsString(authUser));
+        TextMessage request = new TextMessage(objMapper.writeValueAsString(websocketRequest));
+
+        session.sendMessage(request);
+        TextMessage response = futureResponse.get();
+        
+        String HopeconnectionErrorMessage = "session conection fail. session close";
+        String obtainConnectionErrorMesage = response.getPayload();
+        boolean areEqualsConnectionsErrorMessages = HopeconnectionErrorMessage.equals(obtainConnectionErrorMesage);
+        Assertions.assertTrue(areEqualsConnectionsErrorMessages);
     }
 
 }
