b587c2f42cf9718a6baedc3fee59fc06662e8691
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-06 19:43:28 -0500

    [perf] add rol field to credential entity to work correctly with test

--- src/main/resources/TestWebsocketEnviroment.sql
+++ src/main/resources/TestWebsocketEnviroment.sql
@@ -1,4 +1,4 @@
-INSERT INTO credential(id, password, is_enable) VALUES (1, "Test_Developer", 1);
+INSERT INTO credential(id, password, is_enable,rol) VALUES (1, "Test_Developer", 1,"User");
 INSERT INTO member(id, email,user_name, credential_id) VALUES (1, "test_kiyo@dev.com","test_name_1", 1);
 INSERT INTO chat(id, user1_id, user2_id) VALUES (1, 1, 1);
 
