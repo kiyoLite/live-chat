81af1547ecb7d481f621ac52354739461ac80a71
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-08 22:08:16 -0500

    [feat] create a http config to allow all httpRequest

--- src/main/java/dev/kiyolite/live_chat/Config/SecurityConfig.java
+++ src/main/java/dev/kiyolite/live_chat/Config/SecurityConfig.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dev.kiyolite.live_chat.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  *
  * @author soyky
  */
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity(securedEnabled = true , jsr250Enabled = true)
 public class SecurityConfig {
     
+    
+    @Bean
+    public SecurityFilterChain httpConfig(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth ->
+                auth.anyRequest().permitAll()
+        
+        );
+        
+        return http.build();
+    }
 }
