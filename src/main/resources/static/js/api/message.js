c9b55f2541a180a06f658b93c75f1d7bd87bdc9d
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-15 15:12:24 -0500

    [fix] change http method from get to post for make request correctly (not exist a endpoint with get request)

--- src/main/resources/static/js/api/message.js
+++ src/main/resources/static/js/api/message.js
@@ -1,14 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiCallMessagesFromChat = void 0;
 const apiCallMessagesFromChat = function (chatId, starDate, totalMessages) {
     const url = "http://localhost:8080/api/message";
-    const httpMethod = "GET";
+    const httpMethod = "POST";
     const bodyRequest = JSON.stringify({
         "chatId": chatId,
         "startDate": starDate,
         "totalMessages": totalMessages
     });
-    return fetch(url, { method: httpMethod, body: bodyRequest });
+    return fetch(url, { method: httpMethod, headers: { "Content-type": "application/json" }, body: bodyRequest });
 };
-exports.apiCallMessagesFromChat = apiCallMessagesFromChat;
+export { apiCallMessagesFromChat };
--- src/main/resources/static/js/api/message.ts
+++ src/main/resources/static/js/api/message.ts
@@ -1,13 +1,12 @@
 const apiCallMessagesFromChat = function (chatId: number, starDate: string, totalMessages: number) {
     const url = "http://localhost:8080/api/message";
-    const httpMethod = "GET";
+    const httpMethod = "POST";
     const bodyRequest = JSON.stringify({
         "chatId": chatId,
         "startDate": starDate,
         "totalMessages": totalMessages
 
     })
-    return fetch(url, { method: httpMethod, body: bodyRequest });
+    return fetch(url, { method: httpMethod, headers: { "Content-type": "application/json" }, body: bodyRequest });
 }
-
 export { apiCallMessagesFromChat }
