31084a24d637386a0229f9f4819c89c91343605e
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-15 15:42:17 -0500

    [feat] get userName of user by path with pathVariable annotation

--- src/main/java/dev/kiyolite/live_chat/Controller/ChatController.java
+++ src/main/java/dev/kiyolite/live_chat/Controller/ChatController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,8 +27,8 @@ public class ChatController {
 
     private ChatService ChatService;
 
-    @GetMapping("/contact/")
-    public ResponseEntity<List<ChatWrapper>> getContacts(String userNameRequest) {
+    @GetMapping("/contact/{username}")
+    public ResponseEntity<List<ChatWrapper>> getContacts(@PathVariable("username")String userNameRequest) {
         return ChatService.getContactsFromUser(userNameRequest);
     }
 
