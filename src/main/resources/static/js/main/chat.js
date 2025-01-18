e9f8e13c97b5bedfaa1c675049896b20118fd7af
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-18 18:45:06 -0500

    [feat] add  websocket url to establish connection

--- src/main/resources/static/js/main/chat.js
+++ src/main/resources/static/js/main/chat.js
@@ -1,5 +1,5 @@
 import { errorManager, closeConnection, openConnection, recipeMessage } from "../websocket/event-listener.js";
-const websocketUrl = "";
+const websocketUrl = "ws://localhost:8080/chat";
 const socketConnection = new WebSocket(websocketUrl);
 socketConnection.addEventListener("close", closeConnection);
 socketConnection.addEventListener("open", openConnection);
