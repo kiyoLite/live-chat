bda2ddccd567a2580d47ee62995ed9f4de2adfbc
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-19 18:48:42 -0500

    [feat]add enum to represent the websocket response from client side

--- src/main/resources/static/js/websocket/request-type.js
+++ src/main/resources/static/js/websocket/request-type.js
@@ -3,4 +3,11 @@ var WebsocketRequestType;
     WebsocketRequestType["CONNECT"] = "CONNECT";
     WebsocketRequestType["SEND_MESSAGE"] = "SEND_MESSAGE";
 })(WebsocketRequestType || (WebsocketRequestType = {}));
+var WebsocketResponseType;
+(function (WebsocketResponseType) {
+    WebsocketResponseType[WebsocketResponseType["SUCCESSFUL_CONNECT"] = 0] = "SUCCESSFUL_CONNECT";
+    WebsocketResponseType[WebsocketResponseType["BAD_CONNECT"] = 1] = "BAD_CONNECT";
+    WebsocketResponseType[WebsocketResponseType["SUCCESSFUL_SEND"] = 2] = "SUCCESSFUL_SEND";
+    WebsocketResponseType[WebsocketResponseType["BAD_SEND"] = 3] = "BAD_SEND";
+})(WebsocketResponseType || (WebsocketResponseType = {}));
 export { WebsocketRequestType };
--- src/main/resources/static/js/websocket/request-type.ts
+++ src/main/resources/static/js/websocket/request-type.ts
@@ -3,4 +3,11 @@ enum WebsocketRequestType {
     SEND_MESSAGE = "SEND_MESSAGE"
 }
 
+enum WebsocketResponseType {
+    SUCCESSFUL_CONNECT,
+    BAD_CONNECT,
+    SUCCESSFUL_SEND,
+    BAD_SEND
+}
+
 export { WebsocketRequestType }
