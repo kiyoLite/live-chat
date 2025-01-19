588720af79ec06ce1324d140852d78ed64437292
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-19 18:43:27 -0500

    [feat] add enum to represent statues of websocket response

---
+++ src/main/java/dev/kiyolite/live_chat/Enums/WebsocketResponseType.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package dev.kiyolite.live_chat.Enums;
+
+/**
+ *
+ * @author soyky
+ */
+public enum WebsocketResponseType {
+    SUCCESSFUL_CONNECT,
+    BAD_CONNECT,
+    SUCCESSFUL_SEND,
+    BAD_SEND
+    
+    
+    
+}
