0b0b666850ee88dcc8f8fed5ad458c53a9ff9574
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2025-01-20 00:49:39 -0500

    [feat] create a file to manage the message processing from websocket

--- src/main/resources/static/js/websocket/recipe-message-handler.js
+++ src/main/resources/static/js/websocket/recipe-message-handler.js
@@ -1,3 +1,27 @@
-const handlerRecipeMessage = function () {
+import { WebsocketResponseType } from "./status-type.js";
+import { insertMessageWrapperAsLast } from "../view/messageView.js";
+const handlerWebsocketResponse = function (event) {
+    let websocketResponse;
+    try {
+        const response = event.data;
+        websocketResponse = JSON.parse(response);
+    }
+    catch (exeception) {
+        console.error("cast error");
+        websocketResponse = {
+            payload: "",
+            responseType: WebsocketResponseType.BAD_CAST
+        };
+    }
+    return (websocketResponse);
 };
-export { handlerRecipeMessage };
+const handlerResponse = function (response) {
+    const responseType = response.responseType;
+    switch (responseType) {
+        case WebsocketResponseType.SUCCESSFUL_SEND:
+            const message = JSON.parse(response.payload);
+            return insertMessageWrapperAsLast(message);
+        case WebsocketResponseType.BAD_SEND:
+    }
+};
+export { handlerWebsocketResponse as handlerRecipeMessage };
--- src/main/resources/static/js/websocket/recipe-message-handler.ts
+++ src/main/resources/static/js/websocket/recipe-message-handler.ts
@@ -1,7 +1,35 @@
 import { WebsocketResponseType } from "./status-type.js";
+import { websocketResponse } from "../dto/websocketResponse.js";
+import { insertMessageWrapperAsLast } from "../view/messageView.js";
+import { messageWrapper } from "../dto/messageWrapper.js";
 
-const handlerRecipeMessage = function () {
+const handlerWebsocketResponse = function (event: MessageEvent) {
+    let websocketResponse: websocketResponse;
+    try {
+        const response = event.data;
+        websocketResponse = JSON.parse(response);
+    }
+    catch (exeception) {
+        console.error("cast error")
+        websocketResponse = {
+            payload: "",
+            responseType: WebsocketResponseType.BAD_CAST
+        }
+    }
+    return (websocketResponse);
+}
+
+const handlerResponse = function (response: websocketResponse): void {
+    const responseType: WebsocketResponseType = response.responseType;
+    switch (responseType) {
+        case WebsocketResponseType.SUCCESSFUL_SEND:
+            const message: messageWrapper = JSON.parse(response.payload);
+            return insertMessageWrapperAsLast(message);
+        case WebsocketResponseType.BAD_SEND:
+
+    }
 
 }
 
-export { handlerRecipeMessage }
+
+export { handlerWebsocketResponse as handlerRecipeMessage }
